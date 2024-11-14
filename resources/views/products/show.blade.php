8a86ffe1fa699a210bfac4a093cd8cac6227aa58
Ralfs <ralfscd@gmail.com>
2024-11-14 13:59:05 +0200

    w

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,9 +1,10 @@
 
     <div class="container mx-auto p-4">
         <h1 class="text-4xl font-bold">{{ $product->name }}</h1>
-        <p class="mt-4">Quantity:{{ $product->quantity }}</p>
-        <p class="mt-4">Category:{{ $product->category }}</p>
+        <p class="mt-4">Quantity: {{ $product->quantity }}</p>
+        <p class="mt-4">Category: {{ $product->category }}</p>
         <p class="mt-4 font-bold">Price: {{ $product->price }}$</p>
+        <p class="mt-4">Total Price: {{ $product->quantity * $product->price }}$</p>
         <img src="{{ asset('storage/' . $product->image) }}" class="w-full mt-4">
         <div class="mt-4">
             <a href="{{ route('product.edit', $product->id) }}" class="px-4 py-2 bg-blue-500 hover:bg-blue-700 text-white font-bold rounded">Edit</a>
