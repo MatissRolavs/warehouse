857688ff627f13623bebd23ad8fecd04aca61017
matissbby <matissrolavs2005@gmail.com>
2024-11-14 12:53:23 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
     Route::post('/product', [ProductController::class, 'store'])->name('product.store');
     Route::get('/products', [ProductController::class, 'index'])->name('product.index');
-<<<<<<< Updated upstream
     Route::get('/product/{product}', [ProductController::class, 'show'])->name('product.show');
     
  
-=======
-    Route::get('/product', [ProductController::class, 'edit'])->name('product.edit');
->>>>>>> Stashed changes
  
 });
 
