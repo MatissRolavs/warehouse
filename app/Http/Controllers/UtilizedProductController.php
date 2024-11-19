cd5fb7e6b87e0c8c259dedc02dbc445c66ffeaef
matissbby <matissrolavs2005@gmail.com>
2024-11-19 14:22:19 +0200

    sdsd

---
+++ app/Http/Controllers/UtilizedProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUtilizedProductRequest;
+use App\Http\Requests\UpdateUtilizedProductRequest;
+use App\Models\UtilizedProduct;
+
+class UtilizedProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUtilizedProductRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UtilizedProduct $utilizedProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UtilizedProduct $utilizedProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUtilizedProductRequest $request, UtilizedProduct $utilizedProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UtilizedProduct $utilizedProduct)
+    {
+        //
+    }
+}
