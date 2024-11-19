c06ff586cab4c093588d71156f0c63147d712a89
matissbby <matissrolavs2005@gmail.com>
2024-11-19 14:25:12 +0200

    123

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,11 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('product.utilize')" :active="request()->routeIs('product.utilize')">
+                        {{ __('Utilized Products') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('product.create')" :active="request()->routeIs('product.create')">
                         {{ __('Create Product') }}
                     </x-nav-link>
